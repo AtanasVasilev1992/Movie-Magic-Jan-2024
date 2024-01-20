b29fbf2be2542b2b1e0d0bf4a2332164bfceb41e
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-01-20 14:51:31 +0200

    Add movies id and details linki

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,4 +1,5 @@
 const movies = [{
+    _id: 1,
     title: 'Jungle Cuise',
     genre: 'Adventure',
     director: 'Jaume Collet-Serra',
@@ -7,6 +8,7 @@ const movies = [{
     rating: '5',
     description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.',
 } , {
+    _id: 2,
     title: 'The Little Marmaid',
     genre: 'Fantasy',
     director: 'Rob Marshall',
@@ -15,6 +17,7 @@ const movies = [{
     rating: '3',
     description: "Description: The youngest of King Triton's daughters, Ariel is a beautiful and spirited young mermaid with a thirst for adventure. Longing to find out more about the world beyond the sea, Ariel visits the surface and falls for the dashing Prince Eric. Following her heart, she makes a deal with the evil sea witch, Ursula, to experience life on land.",
 } , {
+    _id: 3,
     title: 'Home Alone',
     genre: 'Comedy',
     director: 'Chris Columbus',
@@ -29,5 +32,6 @@ exports.getAll = () => {
 }
 
 exports.create = (movieData) => {
+    movieData._id = movies[movies.length - 1]._id + 1;
     movies.push(movieData);
 }
