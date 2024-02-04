c795e69c51ea805c775d91c600f38fda231cbfe4
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-02-04 19:00:52 +0200

    Add registreted user to DB

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,14 @@
 const routher = require("express").Router();
+const authService = require('../services/authService')
 
 routher.get('/register', (req, res)=>{
     res.render('auth/register');
-})
+});
+
+routher.post('/register', async (req,res)=>{
+    const userData = req.body;
+    await authService.register(userData);
+    res.redirect('/auth/login');
+});
 
 module.exports = routher;
